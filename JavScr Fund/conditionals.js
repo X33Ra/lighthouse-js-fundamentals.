33ecfc6d804d27ab64bc0e3f80a9d8b8ace768a6
MrChan <mrkcwayne@gmail.com>
2023-04-22 18:31:30 -0600

    commit

--- JavScr Fund/conditionals.js
+++ JavScr Fund/conditionals.js
@@ -2,6 +2,7 @@
 
 const raining = true;
 const cold = false;
+const temperature = 12;
 
 if (raining) {
   console.log("Don't forget your umbrella!");
@@ -12,4 +13,14 @@ if (cold) {
 }
 
 console.log("Now you're ready to go outside!");
+// additional prompt from compass
 
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
