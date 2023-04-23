0cd4c979ebfeb3d9d52f8bb947189dfc9d2d626e
MrChan <mrkcwayne@gmail.com>
2023-04-22 23:24:26 -0600

    commit

--- JavScr Fund/Conditionals-whichSchool.js
+++ JavScr Fund/Conditionals-whichSchool.js
@@ -1,16 +1,14 @@
 //In this challenge, we will write some logic to help learners decide which school they should attend.
  
-const whichSchool = function(18){
+const whichSchool = function(age) {
 // code here
-if (whichSchool <13){
+if (whichSchool < 13){
   console.log('you are currently', whichSchool,'you belong to Elementary school')
-  else if(whichSchool >= 13 , <= 18){
+  } else if(whichSchool >= 13 && whichSchool <= 18) {
     console.log('You are currently', whichSchool, ' you belong to Second School')
-  }
-  else{
+  } else {
     console.log('you do not belong in any groups do try Lighthouse')
   } 
-  
 }
 
-}
+whichSchool(35)
