e87579bb570727acf66b1eb9770b39ec47c8a396
MrChan <mrkcwayne@gmail.com>
2023-04-26 01:09:10 -0600

    do it my fren

--- JavScr Fund/ice cream.js
+++ JavScr Fund/ice cream.js
@@ -1,4 +1,9 @@
-const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
-Array.push("root beer");
+let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
 console.log(iceCreamFlavours);
-
+//this puts another stuff in the array
+iceCreamFlavours.push("root beer");
+//return
+console.log(iceCreamFlavours)
+console.log(iceCreamFlavours[0])
+console.log(iceCreamFlavours[5])
+console.log(iceCreamFlavours.length)
--- JavScr Fund/test.js
+++ JavScr Fund/test.js
@@ -1,25 +1,8 @@
-const stations = [
-  ['Big Bear Donair', 10, 'restaurant'],
-  ['Bright Lights Elementary', 50, 'school'],
-  ['Moose Mountain Community Centre', 45, 'community centre']
-];
-// space
-//space
-function chooseStations (stations) {
-  const goodStations = [];
-  for (const station of stations) {
-    const capacity = station[1];
-    if(capacity >= 20) {
-      const type = station[2];
-      if(type === 'school' || type === "community centre") {
-        goodStations.push(station[0]);
-      }
-    }
-  }
- 
+const seasons = ["spring", "summer", "fall"];
+const length = seasons.length;
 
-  return goodStations;
+seasons.push("winter");
+
+console.log(length);
+console.log(seasons.length);
   
-}
- console.log(goodStations(stations));
-
