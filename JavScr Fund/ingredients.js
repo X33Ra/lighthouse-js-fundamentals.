14f9f7fb6732fbd9e95d8910b195d0f0e256346c
MrChan <mrkcwayne@gmail.com>
2023-04-26 23:16:38 -0600

    comute

--- JavScr Fund/ingredients.js
+++ JavScr Fund/ingredients.js
@@ -1,7 +1,11 @@
 const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
 
 // Write a while loop that prints out the contents of ingredients:
-
+let i = 0;
+while (i < ingridients.length) {
+  console.log(ingridients[i]);
+  i++;
+}
 // Write a for loop that prints out the contents of ingredients:
 
 // Write any loop (while or for) that prints out the contents of ingredients backwards:
