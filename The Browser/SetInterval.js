28ed6947bfa58508a7bf10cd9b8f3a16f1dec999
MrChan <mrkcwayne@gmail.com>
2023-04-28 11:11:08 -0600

    Gottem

---
+++ The Browser/SetInterval.js
@@ -0,0 +1,23 @@
+{ const sayHello = function() {
+  alert("Hello");
+}
+const timer = setInterval(sayHello, 3000);
+}
+
+
+{ clearInterval(timer);
+
+const sayHello = function() {
+  alert("Hello");
+}
+const timer = setInterval(sayHello, 3000);
+}
+
+{
+  const sayHello = function() {
+    alert("Hello");
+  }
+  const timer = setInterval(sayHello, 3000);
+  
+  clearInterval(timer);
+}
